b1f412872e0bc5889b9b5141ed6f4b4c45940587
Probytech <tolikshift@gmail.com>
2024-07-23 10:04:18 +0300

    Show image in index

--- core/views/components/index.blade.php
+++ core/views/components/index.blade.php
@@ -108,6 +108,7 @@
 													<svg class="checkbox-mark" enable-background="new 0 0 515.556 515.556" height="512" viewBox="0 0 515.556 515.556" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m0 274.226 176.549 176.886 339.007-338.672-48.67-47.997-290.337 290-128.553-128.552z" fill="white"/></svg>
 												</div>
 											</label>
+											<img :src="instance[field.db_title]" v-else-if="field.type == 'photo'" class="td-content-image" />
 											<span v-text="instance[field.db_title]" v-else></span>
 										</div>
 									</td>
