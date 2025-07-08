1acf6be53f77f4ed80febf58c537a4ede4fc57f0
Probytech <tolikshift@gmail.com>
2025-07-08 11:27:53 +0300

    Add Translatable trait

---
+++ core/Proadmin/Models/Translator/TranslatorBuilder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models\Translator;
+
+use Illuminate\Database\Eloquent\Builder;
+use Request;
+
+class TranslatorBuilder extends Builder
+{
+    public function get($columns = ['*'])
+    {
+        $this->makeFieldsLanguagable();
+
+        $models = parent::get($columns);
+
+        if (Request::method() == 'GET') {
+
+            foreach ($models as $model) {
+
+                $model->removeLanguagesFromFields();
+            }
+        }
+
+        return $models;
+    }
+
+    public function makeFieldsLanguagable()
+    {
+        if ($this->query->columns) {
+
+            foreach ($this->query->columns as &$column) {
+
+                $column = $this->model->tr($column);
+            }
+        }
+
+        if ($this->query->orders) {
+
+            foreach ($this->query->orders as &$order) {
+
+                if (isset($order['column'])) {
+                    $order['column'] = $this->model->tr($order['column']);
+                }
+            }
+        }
+
+        if ($this->query->wheres) {
+
+            foreach ($this->query->wheres as &$where) {
+
+                if (isset($where['column'])) {
+
+                    $where['column'] = $this->model->tr($where['column']);
+                }
+            }
+        }
+    }
+}
