c8b592edba3fb18f1ba155a9c639efdf636561fc
Probytech <tolikshift@gmail.com>
2025-08-20 19:18:08 +0300

    Update 2022_08_27_000003_create_single_fields_table.php

--- database/migrations/2022_08_27_000003_create_single_fields_table.php
+++ database/migrations/2022_08_27_000003_create_single_fields_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Proadmin\Models\Language;
+use Probytech\Proadmin\Models\Language;
 
 return new class extends Migration
 {
@@ -49,7 +49,7 @@ return new class extends Migration
 		$languages = Language::get();
 
 		foreach ($languages as $lang) {
-			
+
 			Schema::dropIfExists("single_fields_{$lang->tag}");
 		}
 	}
