0050494e640eec2470851340037ff2ab03e0ef4b
Probytech <tolikshift@gmail.com>
2026-08-05 12:02:40 +0300

    Update dependencies for Laravel 13

--- database/migrations/2022_08_27_000006_create_roles_table.php
+++ database/migrations/2022_08_27_000006_create_roles_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
 		Schema::create('roles', function (Blueprint $table) {
 			$table->id();
 			$table->string('title');
-			$table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-			$table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+			$table->timestamp('created_at')->useCurrent();
+			$table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
 		});
 
 		DB::table('roles')->insert([
